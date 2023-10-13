65009a13957a810bb677cd56c1d29c13ffecfcb1
Sanshai <vbn-134@yandex.ru>
2023-10-13 21:24:32 +0300

    написание функции, импорт

--- src/components/ShopItemFunc/ShopItemFunc.jsx
+++ src/components/ShopItemFunc/ShopItemFunc.jsx
@@ -1,23 +1,23 @@
 import './ShopItemFunc.css';
 
 export const ShopItemFunc = (props) => {
-    const { brand, title, description, descriptionFull, price, currency } =
-        props;
+    const { item } = props;
+        console.log(item);
     return (
         <article>
             <div className='main-content'>
-                <h2>{brand}</h2>
-                <h1>{title}</h1>
-                <h3>{description}</h3>
-                <div className='description'>{descriptionFull}</div>
+                <h2>{item.brand}</h2>
+                <h1>{item.title}</h1>
+                <h3>{item.description}</h3>
+                <div className='description'>{item.descriptionFull}</div>
                 <div className='highlight-window mobile'>
                     <div className='highlight-overlay'></div>
                 </div>
                 <div className='divider'></div>
                 <div className='purchase-info'>
                     <div className='price'>
-                        {currency}
-                        {price}
+                        {item.currency}
+                        {item.price}
                     </div>
                     <button>Добавить в корзину</button>
                 </div>
